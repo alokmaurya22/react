c1c0a8887ddcc3f3fb9670697012c46d1801dca3
Alok Maurya <alokmauryaraj2003@gmail.com>
2024-09-21 13:30:23 +0530

    Coustom React

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  //let counter = 0;
+  const addValue = ()=>{
+    //console.log("Clicked Value added", Math.random());
+    // counter = counter+1;
+    // setCounter(counter)
+    if (counter<10) {
+      setCounter(counter+=1)
+    }
+    console.log("Counter is : ", counter);
+  }
+  
+  const removeValue = ()=>{
+    if (counter>=1) {
+      setCounter(counter-=1);
+    }
+    console.log("Counter is : ", counter);
+  }
+  return (
+    <>
+      <h1>Hii React</h1>
+      <h2>Value : {counter}</h2>
+      <button onClick = {addValue}>Add Value</button>
+      <br />
+      <button onClick = {removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
