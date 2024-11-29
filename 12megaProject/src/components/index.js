505de075045cfbb8eae3f14e9487f6d7655a0e51
Alok Maurya <alokmauryaraj2003@gmail.com>
2024-11-29 12:49:20 +0530

    RTE and post form done

--- 12megaProject/src/components/index.js
+++ 12megaProject/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo.jsx";
 import LogoutButton from "./Header/LogoutButton";
+import RTE from "./RTE.jsx";
 
-export { Header, Footer, Container, Logo, LogoutButton };
+export { Header, Footer, Container, Logo, LogoutButton, RTE };
